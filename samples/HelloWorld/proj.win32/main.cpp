be085101eb859675c35bac3f12fe5e2fa2c4084b
unknown <uidt4223@TML5321G.cw01.contiwan.com>
2013-02-18 20:06:55 +0200

    add console and route stderr/out to it

--- samples/HelloWorld/proj.win32/main.cpp
+++ samples/HelloWorld/proj.win32/main.cpp
@@ -3,7 +3,65 @@
 #include "AppDelegate.h"
 #include "CCEGLView.h"
 
+#include <windows.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
+#include <iostream>
+#include <fstream>
+
 USING_NS_CC;
+
+#ifndef _USE_OLD_IOSTREAMS
+using namespace std;
+#endif
+
+void setupConsole()
+{
+	int consoleHandle;
+	long osFileHandle;
+	CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+	FILE *fp;
+
+	//search for parent console
+	BOOL consoleReady = AttachConsole(ATTACH_PARENT_PROCESS);
+	if(!consoleReady)
+	{
+		//how about allocating a new one
+		consoleReady = AllocConsole();
+	}
+	if(consoleReady)
+	{
+		//update buffer size
+		GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &consoleInfo);
+		consoleInfo.dwSize.Y = 1200;
+		SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), consoleInfo.dwSize);
+
+		//redirect unbuffered STDOUT to the console
+		osFileHandle = (long)GetStdHandle(STD_OUTPUT_HANDLE);
+		consoleHandle = _open_osfhandle(osFileHandle, _O_TEXT);
+		fp = _fdopen(consoleHandle, "w");
+		*stdout = *fp;
+		setvbuf(stdout, NULL, _IONBF, 0);
+
+		//redirect unbuffered STDIN to the console
+		osFileHandle = (long)GetStdHandle(STD_INPUT_HANDLE);
+		consoleHandle = _open_osfhandle(osFileHandle, _O_TEXT);
+		fp = _fdopen(consoleHandle, "r");
+		*stdin = *fp;
+		setvbuf(stdin, NULL, _IONBF, 0);
+
+		//redirect unbuffered STDERR to the console
+		osFileHandle = (long)GetStdHandle(STD_ERROR_HANDLE);
+		consoleHandle = _open_osfhandle(osFileHandle, _O_TEXT);
+		fp = _fdopen(consoleHandle, "w");
+		*stderr = *fp;
+		setvbuf(stderr, NULL, _IONBF, 0);
+
+		//make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog point to console as well
+		ios::sync_with_stdio();
+	}
+}
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
                        HINSTANCE hPrevInstance,
@@ -12,6 +70,10 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
+
+#ifdef _DEBUG
+	setupConsole();
+#endif
 
     // create the application instance
     AppDelegate app;
